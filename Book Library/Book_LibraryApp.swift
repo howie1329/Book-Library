b2be139c2eba72bb300aaaeff53f89e16e29d5f2
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-02-13 23:12:45 -0500

    Initial Commit

---
+++ Book Library/Book_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_LibraryApp.swift
+//  Book Library
+//
+//  Created by Howard Thomas on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Book_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
